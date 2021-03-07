e415a62f7e2f9094065dbbd40123b57acae4afa0
johnnylyimo <johnnylyimo@gmail.com>
2021-03-07 16:03:21 +0300

    Added Scaffold appBar property

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,6 +8,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
