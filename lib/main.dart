29e387a3c0c19cc2dc3b71fe89f094cb4332672c
johnnylyimo <johnnylyimo@gmail.com>
2021-03-07 17:52:57 +0300

    Added async to main function

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
