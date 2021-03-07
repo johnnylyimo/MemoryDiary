c591cbf73e806598e4c293043377ce5fbb1acdf2
johnnylyimo <johnnylyimo@gmail.com>
2021-03-07 17:51:52 +0300

    Imported hive_flutter package in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:memory_diary/screens/home.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
   runApp(MyApp());
