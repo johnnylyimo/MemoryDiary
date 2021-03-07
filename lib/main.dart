08f87214e8166de7ea41bef508b66651a780183c
johnnylyimo <johnnylyimo@gmail.com>
2021-03-07 16:22:30 +0300

    Added MaterialApp debugShowCheckedModeBanner and assigned to false

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MemoryDiary',
       theme: ThemeData(
         primarySwatch: Colors.blue,
