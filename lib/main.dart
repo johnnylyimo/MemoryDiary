cc0f5cdd59e8cb10638dab93cb140a61f66c1f55
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-04 01:40:03 +0300

    imported path_provider

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@
 import 'package:flutter/material.dart';
 import 'package:memory_diary/screens/home.dart';
 import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
