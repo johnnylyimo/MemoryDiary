4011c5cb7dede7aac75f15cf24274494ac3f8628
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-04 01:47:05 +0300

    Created NewMemory Stateful widget

--- lib/screens/new_memory.dart
+++ lib/screens/new_memory.dart
@@ -1,1 +1,13 @@
+class NewMemory extends StatefulWidget {
+  const NewMemory({Key key}) : super(key: key);
 
+  @override
+  _NewMemoryState createState() => _NewMemoryState();
+}
+
+class _NewMemoryState extends State<NewMemory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
