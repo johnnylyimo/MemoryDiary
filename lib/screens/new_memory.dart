c4dd88f491f0c87d7f47099f394f5c8fda886122
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-04 01:50:02 +0300

    Added AppBar centerTitle property

--- lib/screens/new_memory.dart
+++ lib/screens/new_memory.dart
@@ -11,7 +11,9 @@ class _NewMemoryState extends State<NewMemory> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+      ),
     );
   }
 }
