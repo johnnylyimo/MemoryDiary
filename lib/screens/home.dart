7f237744da96a439c6f132d8c65487bfdb2f6c13
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-04 03:09:17 +0300

    Wrapped Container with Align widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -28,13 +28,15 @@ class _HomeState extends State<Home> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  color: Colors.blue.shade100,
-                  margin: EdgeInsets.all(10.0),
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'WORD OF THE DAY\nToday will be better than yesterday',
-                    textAlign: TextAlign.center,
+                Align(
+                  child: Container(
+                    color: Colors.blue.shade100,
+                    margin: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'WORD OF THE DAY\nToday will be better than yesterday',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
                 Text(
