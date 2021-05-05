cb41a3bdcae814384400c1bf3f5a3f12bf88b212
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-06 01:33:12 +0300

    Updated container child value

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -66,7 +66,7 @@ class _HomeState extends State<Home> {
                 ),
                 Container(
                   height: MediaQuery.of(context).size.height,
-                  child: box.length != 0
+                  child: Memorybox.length != 0
                       ? ListView.builder(
                           itemCount: Memorybox.length,
                           itemBuilder: (context, index) {
