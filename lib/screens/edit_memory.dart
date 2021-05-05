e5edec42f6c24b9aa31fe9597dd9f7aceddd3fd4
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-05 18:36:12 +0300

    Created EditMemory Stateful widget

--- lib/screens/edit_memory.dart
+++ lib/screens/edit_memory.dart
@@ -1,1 +1,13 @@
+class EditMemory extends StatefulWidget {
+  const EditMemory({Key key}) : super(key: key);
 
+  @override
+  _EditMemoryState createState() => _EditMemoryState();
+}
+
+class _EditMemoryState extends State<EditMemory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
