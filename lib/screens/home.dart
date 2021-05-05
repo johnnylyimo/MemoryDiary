6086a11cdd4bef99a80d56d7c5759847bbb75605
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-05 16:29:16 +0300

    Added Container to Column Children

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -51,7 +51,8 @@ class _HomeState extends State<Home> {
                 ),
                 SizedBox(
                   height: 20.0,
-                )
+                ),
+                Container()
               ],
             ),
           ),
