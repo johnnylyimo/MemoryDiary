f29d52d35c4db5460c4f8d6329461e2a2a366dc0
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-05 16:30:56 +0300

    Assigned ListView.builder to Container child

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -54,6 +54,7 @@ class _HomeState extends State<Home> {
                 ),
                 Container(
                   height: MediaQuery.of(context).size.height,
+                  child: ListView.builder(),
                 )
               ],
             ),
