53a8b0144beb9591be139c027948a8c8bfeb5a93
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-05 19:54:23 +0300

    Imported flutter_riverpod package

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:memory_diary/screens/home.dart';
