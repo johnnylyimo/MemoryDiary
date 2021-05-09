0d36c95b1bd87443bd9da15270fcf54f8e7ed861
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-09 20:30:27 +0300

    Updated primarySwatch color to purple

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'MemoryDiary',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
         scaffoldBackgroundColor: Colors.purple.shade100,
       ),
       routes: {
