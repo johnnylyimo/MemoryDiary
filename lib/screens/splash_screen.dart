aa81204e742dfee7f68d3f6ef4ff47f3206592c6
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-09 20:14:46 +0300

    Called Future.delayed method

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -9,6 +9,16 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PianoPlayGround(),
+        ),
+      );
+    });
+  }
   }
 
   @override
