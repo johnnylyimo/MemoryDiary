f79805b580061bc5ecf963aa132512dde5f0ceae
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-09 20:23:15 +0300

    Added Column widget to SafeArea child

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -24,7 +24,9 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(),
+      body: SafeArea(
+        child: Column()
+      ),
     );
   }
 }
