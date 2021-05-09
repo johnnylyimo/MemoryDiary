79e9ce14d42300d6d0cd18136c25ffe3e5622644
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-09 20:08:51 +0300

    Created SplashScreen Stateful widget

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,11 @@
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
